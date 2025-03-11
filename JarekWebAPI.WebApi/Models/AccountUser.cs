b17bf5fca75277b01670344b88357765f198d2ad
Jarek van Dijk <j.vandijk30@student.avans.nl>
2025-03-11 14:35:09 +0100

    v5

--- JarekWebAPI.WebApi/Models/AccountUser.cs
+++ JarekWebAPI.WebApi/Models/AccountUser.cs
@@ -7,8 +7,8 @@ namespace JarekWebAPI.WebApi
     {
         public Guid Id { get; set; }
         [Required]
-        public required string UserName { get; set; }
+        public string? UserName { get; set; }
         [Required]
-        public required string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
